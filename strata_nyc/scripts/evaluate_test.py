d841fea69fefa61b7ff3fb14ccd0f2b8cf55a286
Sourav Dey <sdey@manifold.ai>
2019-09-23 15:48:08 -0700

    Changing test absolute directory

--- strata_nyc/scripts/evaluate_test.py
+++ strata_nyc/scripts/evaluate_test.py
@@ -4,7 +4,7 @@ from click.testing import CliRunner
 from strata_nyc.scripts.evaluate import evaluate
 
 
-@pytest.mark.parametrize("config_file", [("/mnt/configs/test_config.yml")])
+@pytest.mark.parametrize("config_file", [("configs/test_config.yml")])
 def test_evaluate(config_file):
     runner = CliRunner()
     result = runner.invoke(evaluate, [config_file])
